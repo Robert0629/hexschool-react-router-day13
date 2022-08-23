4a09bc8e1b39066d67b5b40e7af15c47893c62c8
Robert <rl.84@icloud.com>
2022-08-23 15:08:45 +0800

    [feat] 新增動態路由練習

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   Routes,
   Route,
   useNavigate,
+  Outlet,
+  useParams,
 } from "react-router-dom";
 
 const Todo = () => {
@@ -32,6 +34,22 @@ const Register = () => {
   return <p>這是註冊頁面</p>;
 };
 
+const Post = () => {
+  return (
+    <div>
+      <NavLink to=":post111">
+        <h2>Post 詳細頁面1</h2>
+      </NavLink>
+      <Outlet />
+    </div>
+  );
+};
+
+const PostId = () => {
+  let params = useParams();
+  return <p>PostId: {params.postId}</p>;
+};
+
 function App() {
   return (
     <div className="container">
@@ -49,12 +67,18 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/todo" element={<Todo />} />
+          <Route path="/post" element={<Post />} />
+           <Route path=":postId" element={<PostId />} />
+          <Route/>
         </Routes>
         {/* 練習區 */}
       </HashRouter>
